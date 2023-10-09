7390af017d242254d9660adb3ec6b75e1b3930de
sgarbossabeatriz <110116950+sgarbossabeatriz@users.noreply.github.com>
2023-10-09 10:47:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function tocaSomPom () { 
+    document.querySelector('#som_tecla_pom').play();
+}
+
+ 
+document.querySelector('.tecla_pom').onclick= tocaSomPom;
+
+function tocaSomClap () { 
+    document.querySelector('#som_tecla_Clap').play();
+}
+
+ document.querySelector('.tecla_Clap').onclick= tocaSomClap;
+
+ function tocaSomTim() { 
+    document.querySelector('#som_tecla_Tim').play();
+}
+
+ document.querySelector('.tecla_Tim').onclick= tocaSomTim;
+
+ 
